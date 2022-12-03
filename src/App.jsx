f216d12916b436af62ab4380b889735de8cae243
JulianBarbossa9 <julianbarbossa@hotmail.com>
2022-12-03 17:54:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+  return (
+    <div className='container '>
+      <h1>Select your Birthday</h1>
+    </div>
+  )
+}
+
+export default App
